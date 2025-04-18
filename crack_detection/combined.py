78003ea599d391c2fa74fa041f0cbdf2df18515b
ejVasco <111936685+ejVasco@users.noreply.github.com>
2025-04-18 10:18:51 -0700

    started new combined file

---
+++ crack_detection/combined.py
@@ -0,0 +1,13 @@
+from colorama import Fore, Style # color for error message
+import cv2
+import time
+import torch
+import numpy as np
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
+#------------------------------------------------
+def main(input:str, mode:int)
+#------------------------------------------------
+# this is used to handle input arguments
+if __name__ == "__main__":
+  import sys
+  
