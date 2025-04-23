104fe5aae652280c856b951a8a6c813cbd622d5c
ejVasco <111936685+ejVasco@users.noreply.github.com>
2025-04-22 18:49:26 -0700

    input handling started

--- crack_detection/combined.py
+++ crack_detection/combined.py
@@ -5,9 +5,32 @@ import torch
 import numpy as np
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 #------------------------------------------------
-def main(input:str, mode:int)
+def main(input:str, mode:int):
+    if mode == 0:
+        print('main in default mode confirmed')
+    else:
+        # print("mode is " + mode)
+        pass
 #------------------------------------------------
-# this is used to handle input arguments
+# strings for outputting
+help_s = 'help placeholder'
+help_args = ['h', 'help', '-h', '-help', '--h', '--help']
+# functions to handle input
+def file_exists(input:str) -> bool:
+    return True
 if __name__ == "__main__":
-  import sys
-  
+    import sys
+    if len(sys.argv) < 2:
+        print("Too few arguments")
+        print(help_s)
+    elif len(sys.argv) == 2:
+        if sys.argv[1] in help_args:
+            print(help_s)
+        elif file_exists(sys.argv[1]):
+            print('main in default mode?')
+            main(sys.argv[1], 0)
+    elif file_exists(sys.argv[1]) and sys.argv[2].isdigit():
+        pass
+    else:
+        print("Invalid usage")
+        print(help_s)
